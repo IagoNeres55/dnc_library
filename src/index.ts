8393e7d187d715f315770ca71567fee99682cda3
IagoNeres55 <iagoneres90@gmail.com>
2024-10-14 21:53:19 -0300

    config: cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-// import cors, { CorsOptions } from "cors";
+import cors, { CorsOptions } from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.router";
 import booksRouter from "./routes/books.router";
@@ -8,14 +8,14 @@ import conectionDB from "./config/conection.mongo";
 dotenv.config();
 const port = process.env.PORT || 3001;
 
-// const corsOptions: CorsOptions = {
-//   origin: "http://example.com", // Define a origem permitida
-//   methods: ["GET", "POST"], // Define métodos permitidos
-//   allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
-// };
+const corsOptions: CorsOptions = {
+  origin: "http://example.com", // Define a origem permitida
+  methods: ["GET", "POST"], // Define métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+};
 
 const app: Express = express();
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 conectionDB();
 
